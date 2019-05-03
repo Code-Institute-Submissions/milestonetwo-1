945f3e7da0024e4a8ef93467d91a36e06cd306b4
Jason Bailey <jaybailey@outlook.com>
2019-05-03 17:47:31 +0000

    added causeselect fuction to wgraph.js to display school data from csv file

--- assets/js/wgraph.js
+++ assets/js/wgraph.js
@@ -6,6 +6,18 @@ queue()
 function makeGraphs(error, causeData) {
     var ndx = crossfilter(causeData);
 
-     console.log(causeData);
+    causeselect(ndx);
+
+     //console.log(causeData);
      dc.renderAll();
+}
+
+//Drop down selector for selection of school
+function causeselect(ndx) {
+    var dim = ndx.dimension(dc.pluck('school'));
+    var group = dim.group();
+    
+    dc.selectMenu("#make-selector")
+        .dimension(dim)
+        .group(group);
 }
